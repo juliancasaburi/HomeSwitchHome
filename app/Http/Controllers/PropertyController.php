8b47eba5234f93903946595f8fbe5eb155750d10
Julian Casaburi <48498042+juliancasaburi@users.noreply.github.com>
2019-05-15 00:08:32 -0300

    Add property id validation to PropertyController
    GET parameter id (/property?id=) now gets validated before returning property view.
    Redirects to 404 error page if id doesn't exist.

--- app/Http/Controllers/PropertyController.php
+++ app/Http/Controllers/PropertyController.php
@@ -9,7 +9,7 @@ class PropertyController extends Controller
 {
 
     /**
-     * Show the application dashboard.
+     * Show the property list.
      *
      * @return \Illuminate\Http\Response
      */
@@ -17,6 +17,24 @@ class PropertyController extends Controller
     {
          $property = Property::where('id', Request()->id)
             ->get()->first();
+
+        // If property id doesn't exist, show error 404 page
+        if(empty($property)) {
+            abort(404);
+        }
+
+        // Property exists
         return view('property')->with ('property', $property);
+    }
+
+    /**
+     * Show the property grid.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function showGrid()
+    {
+        $properties = Property::paginate(2);
+        return view('properties')->with ('properties', $properties);
     }
 }
