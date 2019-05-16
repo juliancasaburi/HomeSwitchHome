bb32d75d58dc99862695ced8b24c7a36bde4e81e
Julian Casaburi <48498042+juliancasaburi@users.noreply.github.com>
2019-05-16 02:58:19 -0300

    Add Auction list to admin's dashboard menu

---
+++ resources/views/admin-auction-list.blade.php
@@ -0,0 +1,116 @@
+@extends('layouts.admin-dashboard-layout')
+
+@section('title', '- Admin Dashboard - Listado de subastas')
+
+@section('css')
+    <link rel="stylesheet" type="text/css" href="{{ asset('libs/datatables/css/dataTables.bootstrap4.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('libs/datatables/css/buttons.bootstrap4.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('libs//datatables/css/select.bootstrap4.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('libs/datatables/css/fixedHeader.bootstrap4.css') }}">
+@endsection
+
+@section('content')
+    <!-- ============================================================== -->
+    <!-- main wrapper -->
+    <!-- ============================================================== -->
+    <div class="dashboard-main-wrapper">
+    @include('layouts.partials.admin-sidebar')
+    <!-- ============================================================== -->
+        <!-- wrapper  -->
+        <!-- ============================================================== -->
+        <div class="dashboard-wrapper">
+            <div class="container-fluid  dashboard-content">
+                <!-- ============================================================== -->
+                <!-- pageheader  -->
+                <!-- ============================================================== -->
+                <div class="row">
+                    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                        <div class="page-header">
+                            <h2 class="pageheader-title">Admin Dashboard</h2>
+                            <p class="pageheader-text">Nulla euismod urna eros, sit amet scelerisque torton lectus vel mauris facilisis faucibus at enim quis massa lobortis rutrum.</p>
+                            <div class="page-breadcrumb">
+                                <nav aria-label="breadcrumb">
+                                    <ol class="breadcrumb">
+                                        <li class="breadcrumb-item"><a href={{ url('admin/dashboard') }} class="breadcrumb-link">Dashboard</a></li>
+                                        <li class="breadcrumb-item"><a href="#" class="breadcrumb-link">Subastas</a></li>
+                                        <li class="breadcrumb-item active" aria-current="page">Ver listado</li>
+                                    </ol>
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- ============================================================== -->
+                <!-- end pageheader  -->
+                <!-- ============================================================== -->
+                <div class="row">
+                    <!-- ============================================================== -->
+                    <!-- users table  -->
+                    <!-- ============================================================== -->
+                    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                        <div class="card">
+                            <h5 class="card-header">Listado de Subastas</h5>
+                            <div class="card-body">
+                                <div class="table-responsive">
+                                    <table class="table table-striped table-bordered first">
+                                        <thead>
+                                        <tr>
+                                            <th></th>
+                                            <th>ID</th>
+                                            <th>Semana ID</th>
+                                            <th>Propiedad</th>
+                                            <th>Precio Inicial</th>
+                                            <th>Plazo inscripción</th>
+                                            <th>Comienza</th>
+                                            <th>Finaliza</th>
+                                            <th>Creada</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        @foreach ($auctions as $a)
+                                            <tr>
+                                                <td><button class="btn-primary"><i class="fas fa-tools"></i>Administrar</button></td>
+                                                <td>{{ $a->id }}</td>
+                                                <td>{{ $a->semana_id }}</td>
+                                                <td><a href={{ url('property?id=').$a->week->property->id }}>ID: {{ $a->week->property->id }} "{{ $a->week->property->nombre }}"</a></td>
+                                                <td>{{ $a->precio_inicial }}</td>
+                                                <td>--</td>
+                                                <td>{{ $a->inicio }}</td>
+                                                <td>{{ $a->fin }}</td>
+                                                <td>{{ $a->created_at }}</td>
+                                            </tr>
+                                        @endforeach
+                                        </tbody>
+                                        <tfoot>
+                                        <tr>
+                                            <th></th>
+                                            <th>ID</th>
+                                            <th>Semana ID</th>
+                                            <th>Propiedad</th>
+                                            <th>Precio Inicial</th>
+                                            <th>Plazo inscripción</th>
+                                            <th>Comienza</th>
+                                            <th>Finaliza</th>
+                                            <th>Creada</th>
+                                        </tr>
+                                        </tfoot>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- ============================================================== -->
+                    <!-- end users table  -->
+                    <!-- ============================================================== -->
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- ============================================================== -->
+    <!-- end main wrapper -->
+    <!-- ============================================================== -->
+@endsection
+
+@section('js')
+    <script src="{{ asset('js/dashboard.js') }}"></script>
+@endsection
