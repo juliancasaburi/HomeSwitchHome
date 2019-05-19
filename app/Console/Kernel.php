be02e3d1a220e6254d3dd0c239cf293632e9ce9b
Julian Casaburi <48498042+juliancasaburi@users.noreply.github.com>
2019-05-19 01:23:41 -0300

    Add Scheduled Task to assign Rerservations
    https://laravel.com/docs/5.8/scheduling
    Closing an auction is not implemented yet.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,9 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
+use App\Reservation;
 
 class Kernel extends ConsoleKernel
 {
@@ -24,6 +27,28 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        $schedule->call(function () {
+            $auctionsToBeClosed = DB::table('subastas')->where('fin', '<=', Carbon::now())->get();
+            foreach($auctionsToBeClosed as $a) {
+                if (!DB::table('pujas')->where('subasta_id', $a->id)->get()->isEmpty()) {
+                    $winnerBid = DB::table('pujas')->where('subasta_id', $a->id)->latest()->first();
+                    $winnerUser = DB::table('usuarios')->where('usuario_id', $winnerBid->id)->first();
+                    while (($winnerUser->saldo < $winnerBid->monto) && (DB::table('pujas')->where('subasta_id', $a->id)->get()->except($winnerBid->id)->count() > 0)) {
+                        $winnerBid = DB::table('pujas')->where('subasta_id', $a->id)->except($winnerBid->id)->latest()->first();
+                        $winnerUser = DB::table('usuarios')->where('usuario_id', $winnerBid->id)->first();
+                    }
+                    if ($winnerUser->saldo >= $winnerBid->monto) {
+                        $reservation = new Reservation();
+                        $reservation->usuario_id = $winnerUser->id;
+                        $reservation->valor_reservado = $winnerBid->monto;
+                        $reservation->fecha = Carbon::now();
+                        $reservation->modo_reserva = 0;
+                        $reservation->save();
+                    }
+                }
+            }
+        })->everyMinute();
+
         // $schedule->command('inspire')
         //          ->hourly();
     }
