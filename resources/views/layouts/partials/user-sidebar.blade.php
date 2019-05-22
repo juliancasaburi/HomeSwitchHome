3a00ea4e5ec334ee2bfea0b7ade6c69c2f765349
Julian Casaburi <48498042+juliancasaburi@users.noreply.github.com>
2019-05-22 13:03:18 -0300

    Use Laravel URL Helper
    To generate a fully qualified URL to the given path.
    https://laravel.com/docs/5.8/helpers#method-url

--- resources/views/layouts/partials/user-sidebar.blade.php
+++ resources/views/layouts/partials/user-sidebar.blade.php
@@ -24,10 +24,10 @@
                         <div id="submenu-1" class="collapse submenu" style="">
                             <ul class="nav flex-column">
                                 <li class="nav-item">
-                                    <a class="nav-link" href="/profile/modify-email">Modificar email</a>
+                                    <a class="nav-link" href="{{ url('/profile/modify-email') }}">Modificar email</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" href="/profile/modify-password">Modificar contraseña</a>
+                                    <a class="nav-link" href="{{url('/profile/modify-password')}}">Modificar contraseña</a>
                                 </li>
                             </ul>
                         </div>
