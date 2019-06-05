566b4e96b59e590436691105d58d9ac8ac8b2b60
Julian Casaburi <48498042+juliancasaburi@users.noreply.github.com>
2019-06-05 14:36:08 -0300

    Add a link to log above Register form
    It's handy if you already have an account

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -36,13 +36,15 @@
 @include('layouts.partials.nav')
 
 <div>
-
     @include('layouts.partials.searchForm')
 
     @yield('content')
 
     @include('layouts.partials.footer')
 </div>
+
+<!-- Include per-page JS -->
+@yield('js')
 
 {{-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script --}}
 <!-- JavaScript Libraries -->
