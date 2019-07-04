ddd48e4c91b2e10ff8258f7830b6c57f8ccb5ba4
Julian Casaburi <48498042+juliancasaburi@users.noreply.github.com>
2019-07-04 01:29:26 -0300

    Update faq.blade.php

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -8,8 +8,8 @@
             <div class="row">
                 <div class="col-md-12 col-lg-8">
                     <div class="title-single-box">
-                        <h1 class="title-single">¿Pregunta 1?</h1>
-                        <span class="color-text-a">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
+                        <h1 class="title-single">Preguntas Frecuentes</h1>
+                        <span class="color-text-a">Aut voluptas consequatur unde sed omnis ex placeat quis eos. Aut natus officia corrupti qui autem fugit consectetur quo. Et ipsum eveniet laboriosam voluptas beatae possimus qui ducimus. Et voluptatem deleniti. Voluptatum voluptatibus amet. Et esse sed omnis inventore hic culpa.</span>
                     </div>
                 </div>
                 <div class="col-md-12 col-lg-4">
@@ -26,7 +26,7 @@
                 </div>
             </div>
         </div>
-        <br>
+    </section>
         <div class="container">
             <div class="row">
                 <div class="col-md-12 col-lg-8">
@@ -73,7 +73,6 @@
 
     </section>
     <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
-    <div id="preloader"></div>
 
 
 @endsection
