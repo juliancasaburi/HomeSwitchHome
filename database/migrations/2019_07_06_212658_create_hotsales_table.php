f4deeb7c78448dc941e616e5c63ae5955448f531
AxelRudz <48615927+axelrudz@users.noreply.github.com>
2019-07-06 21:49:52 -0300

    Hotsales migration

---
+++ database/migrations/2019_07_06_212658_create_hotsales_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHotsalesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hotsales', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('semana_id')->unsigned()->index();
+            $table->decimal('precio', 9, 2);
+            $table->dateTime('fecha_inicio');
+            $table->dateTime('fecha_fin');
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('semana_id')->references('id')->on('semanas');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hotsales');
+    }
+}
